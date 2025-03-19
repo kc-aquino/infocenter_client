56674f6026d455909d1e43fe09290d1f208a0529
crystal <66188059+kc-aquino@users.noreply.github.com>
2025-03-19 18:01:52 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import MainLayout from './pages/MainLayout'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <MainLayout >
+
+      </MainLayout>
+    </>
+  )
+}
+
+export default App
