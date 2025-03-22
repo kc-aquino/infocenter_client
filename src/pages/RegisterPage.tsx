69736f6bc099b9d2580143fa4a705cd9e4279878
crystal <66188059+kc-aquino@users.noreply.github.com>
2025-03-22 12:49:17 +0800

    connected api garbage & tsunami

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,5 +1,4 @@
 import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Card, CardContent, CardFooter } from '@/components/ui/card';
