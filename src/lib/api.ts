8aa16f5ff47d6c5399b761d949b078774af8d460
crystal <66188059+kc-aquino@users.noreply.github.com>
2025-03-24 19:32:41 +0800

    connect other advisories to api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,12 @@
 const API_URL = import.meta.env.VITE_API_URL;
-
 export const fetchData = async (endpoint: string) => {
+  if (!API_URL) {
+    console.warn(
+      'VITE_API_URL is not defined. Please check your environment variables.',
+    );
+    return [];
+  }
+
   try {
     const response = await fetch(`${API_URL}/${endpoint}`);
     if (!response.ok) {
@@ -8,7 +14,7 @@ export const fetchData = async (endpoint: string) => {
     }
     return await response.json();
   } catch (error) {
-    console.error("API Fetch Error:", error);
-    throw error;
+    console.error('API Fetch Error:', error);
+    return [];
   }
 };
