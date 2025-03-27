e765d420ea035b9b0d8cef95143694db51bd2bf4
crystal <66188059+kc-aquino@users.noreply.github.com>
2025-03-27 13:31:49 +0800

    fixed button in TrafficPage

--- src/pages/TrafficPage.tsx
+++ src/pages/TrafficPage.tsx
@@ -81,22 +81,22 @@ const TrafficPage = () => {
       <div className="bg-orange-500 text-white flex justify-between items-center rounded-0 md:rounded-xl p-6 m-6 mb-0 h-auto">
         <div className=" sm:flex-row sm:items-center sm:justify-between w-full">
           <div>
-            <h2 className="text-sm font-semibold ">
+            <h2 className="text-sm font-semibold">
               Want real-time traffic updates across Malabon City? Click this to
               get redirected to the official Facebook page.
             </h2>
           </div>
-          <div className="flex items-center justify-between  w-full mt-4 sm:mt-0">
-            <p className="text-sm text-white/90 max-w-xl">
+          <div className="flex flex-col sm:flex-row items-center justify-between w-full mt-4 sm:mt-0 gap-2">
+            <p className="text-sm text-white/90 max-w-xl text-center sm:text-left">
               Malabon City Central Commands and Communications Center
               periodically provides real-time traffic updates through Facebook
-              live.
+              Live.
             </p>
             <Button
-              className="bg-white text-orange-500 font-semibold self-end mr-2 w-1/4 "
+              className="bg-white text-orange-500 font-semibold px-3 py-2 w-full sm:w-auto text-sm text-center whitespace-nowrap"
               onClick={() => window.open(fbPageLink, '_blank')}
             >
-              Click to visit Facebook page
+              Click to visit Facebook Page
             </Button>
           </div>
         </div>
